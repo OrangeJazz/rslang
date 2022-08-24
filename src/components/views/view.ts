376696f04e3feed33e9a6f9af6cd10e9814cb4e0
OrangeJazz <wilinwen@mail.ru>
2022-08-25 00:34:06 +0400

    fix: fix some prettier and package propertyes

--- src/components/views/view.ts
+++ src/components/views/view.ts
@@ -1,15 +1,19 @@
 import { Control } from './control';
+import { Header } from './header/header';
+import { Logo } from './header/logo';
 import { Textbook } from './textbook/textbook';
 
 export class View extends Control {
     header: Control;
     main: Textbook;
     footer: Control;
+    logo: Control;
 
     constructor() {
         super(document.body, 'div');
-        this.header = new Control(this.node, 'header');
+        this.header = new Header(this.node);
         this.main = new Textbook(this.node);
         this.footer = new Control(this.node, 'footer');
+        this.logo = new Logo(this.node);
     }
 }
