b09c023c78cc777b8efd743958bf31e2b83d1bbc
OrangeJazz <wilinwen@mail.ru>
2022-08-25 04:33:47 +0400

    feat: add footer layout view

--- src/components/models/model.ts
+++ src/components/models/model.ts
@@ -1,10 +1,58 @@
 import { Word } from '../types';
 
+interface Link {
+    name: string;
+    link: string;
+}
+
+export interface Card {
+    name: string;
+    content: string;
+    path: string;
+    link: string;
+}
+
 export class Model {
     private backendBaseURL: string;
+    menu: Link[];
+    developers: Link[];
+    features: Card[];
 
     constructor(backendBaseURL = '') {
         this.backendBaseURL = backendBaseURL;
+        this.menu = [
+            { name: 'Главная', link: '1' },
+            { name: 'Учебник', link: '2' },
+            { name: 'Игры', link: '3' },
+            { name: 'Статистика', link: '4' },
+        ];
+        this.developers = [
+            { name: 'Антон', link: 'https://github.com/antonsergeev' },
+            { name: 'Олег', link: 'https://github.com/ali-gator' },
+            { name: 'Мария', link: 'https://github.com/OrangeJazz' },
+        ];
+
+        this.features = [
+            {
+                name: 'Учебник',
+                content: 'Слова в учебнике разбиты на разделы по их сложности. Учи то, что подходит именно тебе!',
+                path: 'learn',
+                link: '',
+            },
+            {
+                name: 'Игры',
+                content: 'С самого детства мы учимся играя. Играй в мини-игры и запоминай новые слова.',
+                path: 'game',
+                link: '',
+            },
+            {
+                name: 'Статистика',
+                content:
+                    'Авторизуйся и отслеживай свой прогресс, чтобы успешно достичь поставленных целей. И помни, постоянная практика - залог успеха!',
+                path: 'stat',
+                link: '',
+            },
+        ];
     }
 
     setBackendBaseURL(backendBaseURL: string) {
---
+++ src/components/views/footer/footer.ts
@@ -0,0 +1,48 @@
+import { Control } from '../control';
+import { Model } from '../../models/model';
+import './footer.scss';
+
+export class Footer extends Control {
+    model: Model;
+    constructor(parentNode: HTMLElement) {
+        super(parentNode, 'footer', 'footer');
+        this.model = new Model();
+        const container = new Control(this.node, 'div', 'container');
+        const wrapper = new Control(container.node, 'div', 'footer__wrapper');
+        const topBlock = new Control(wrapper.node, 'div', 'footer__top-block');
+        const firstColumnDiv = new Control(topBlock.node, 'div');
+        const logoContainer = new Control(firstColumnDiv.node, 'div', 'footer__logo-container');
+        const logoImg = new Control<HTMLImageElement>(logoContainer.node, 'img', 'footer__logo');
+        logoImg.node.src = './assets/img/rsschool.png';
+        logoImg.node.alt = 'RS School logo';
+        const secondColumnDiv = new Control(topBlock.node, 'div');
+        new Control(secondColumnDiv.node, 'h3', 'footer__heading', 'Меню');
+        const menuList = new Control<HTMLUListElement>(secondColumnDiv.node, 'ul', 'footer__list');
+        for (let i = 0; i < this.model.menu.length; i++) {
+            const menuItem = new Control<HTMLElement>(menuList.node, 'li');
+            const menuLink = new Control<HTMLAnchorElement>(
+                menuItem.node,
+                'a',
+                'nav__list-link',
+                `${this.model.menu[i].name}`
+            );
+            menuLink.node.href = this.model.menu[i].link;
+        }
+        const thirdColumnDiv = new Control(topBlock.node, 'div');
+        new Control(thirdColumnDiv.node, 'h3', 'footer__heading', 'Разработчики');
+        const devList = new Control<HTMLUListElement>(thirdColumnDiv.node, 'ul', 'footer__list');
+        for (let i = 0; i < this.model.developers.length; i++) {
+            const devItem = new Control<HTMLElement>(devList.node, 'li');
+            const devLink = new Control<HTMLAnchorElement>(
+                devItem.node,
+                'a',
+                'nav__list-link',
+                `${this.model.developers[i].name}`
+            );
+            devLink.node.href = this.model.developers[i].link;
+        }
+        const bottomBlock = new Control(wrapper.node, 'div', 'footer__bottom-block', '©2022 RS LANG. ');
+        const courseLink = new Control<HTMLAnchorElement>(bottomBlock.node, 'a', '', 'RS School Course.');
+        courseLink.node.href = 'https://rs.school';
+    }
+}
