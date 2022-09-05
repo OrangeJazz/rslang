770f60bf8398f6c23ee99ca9264e9df377a9b44e
Oleg <ogordienov@yandex.ru>
2022-09-05 18:06:27 +0200

    feat: add api class

---
+++ src/components/controllers/api.ts
@@ -0,0 +1,76 @@
+import { User } from '../types';
+
+export class Api {
+    private readonly backendBaseURL: string;
+    jwt: string | null;
+    userId: string | null;
+
+    constructor(backendBaseURL: string) {
+        this.backendBaseURL = backendBaseURL;
+        this.jwt = localStorage.getItem('jwt');
+        this.userId = localStorage.getItem('userId');
+    }
+
+    async signUp({ email, password, name }: User): Promise<{ id: string; email: string; name: string }> {
+        const res = await fetch(`${this.backendBaseURL}users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password, name }),
+        });
+        if (res.ok) {
+            return res.json();
+        }
+        throw new Error(res.status.toString());
+    }
+
+    async signIn({ email, password }: Omit<User, 'name'>): Promise<void> {
+        const res = await fetch(`${this.backendBaseURL}signin`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        if (res.ok) {
+            const { token, userId } = await res.json();
+            this.jwt = token;
+            this.userId = userId;
+            localStorage.setItem('jwt', token);
+            localStorage.setItem('userId', userId);
+            return;
+        }
+        throw new Error(res.status.toString());
+    }
+
+    async getUserInfo(): Promise<void> {
+        const res = await fetch(`${this.backendBaseURL}users/${this.userId}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        });
+        if (res.ok) {
+            return res.json();
+        }
+        throw new Error(res.status.toString());
+    }
+
+    async getUserStatistic(): Promise<void> {
+        const res = await fetch(`${this.backendBaseURL}users/${this.userId}/statistics`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.jwt}`,
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        });
+        if (res.ok) {
+            return res.json();
+        }
+        throw new Error(res.status.toString());
+    }
+}
--- src/components/controllers/controller.ts
+++ src/components/controllers/controller.ts
@@ -2,21 +2,23 @@ import { BACKEND_BASE_URL } from './config';
 import { AudioManager } from './audio-manager';
 import { Model } from '../models/model';
 import { View } from '../views/view';
-// import { StartPage } from '../views/start-page/start-page';
 import { Textbook } from '../views/main/textbook/textbook';
 import { Auth } from '../views/main/auth/auth';
 import { User } from '../types';
+import { Api } from './api';
 
 export class Controller {
     model: Model;
     view: View;
     audioManager: AudioManager;
+    api: Api;
 
     constructor(model: Model, view: View) {
         this.model = model;
         this.view = view;
         this.model.setBackendBaseURL(BACKEND_BASE_URL);
         this.audioManager = new AudioManager();
+        this.api = new Api(BACKEND_BASE_URL);
     }
 
     async start(): Promise<void> {
@@ -27,6 +29,8 @@ export class Controller {
                     const words = await this.model.getWords(group, page);
                     pageView.renderCards(words);
                     pageView.onAudioPlay = (audioNode) => this.audioManager.handle(audioNode);
+                    const userInfo = await this.api.getUserInfo();
+                    console.log(userInfo);
                 };
 
                 pageView.onNewWordsPage(0, 0);
@@ -39,14 +43,12 @@ export class Controller {
                         const formValues = new FormData(e.target as HTMLFormElement);
                         const { email, name, password } = Object.fromEntries(formValues.entries());
                         if (formValues.has('name')) {
-                            await this.model.signUp({ email, name, password } as unknown as User);
+                            await this.api.signUp({ email, name, password } as unknown as User);
                         }
-                        const { token, userId } = await this.model.signIn({
+                        await this.api.signIn({
                             email,
                             password,
                         } as unknown as Omit<User, 'name'>);
-                        localStorage.setItem('jwt', token);
-                        localStorage.setItem('userId', userId);
                         window.location.replace('/');
                     });
                 };
